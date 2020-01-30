cc36816e86fe7f806ff0ee6aa73f207feedfcf8d
wildcard329 <enkiduwildman@gmail.com>
2020-01-30 15:46:36 -0800

    added yup

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -3,16 +3,30 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
-function UserForm() {
+function UserForm({ values, errors, touched }) {
     
     return (
-        <div className='userForm'>
-            <Form>
-                    <Field type="text" name="name" placeholder="Name"/>
-                    <Field type="text" name="email" placeholder="email"/>
-                    <Field type="text" name="password" placeholder="Password"/>
+        <Form>
+            <div>
+                {touched.name && errors.name && <p>{errors.name}</p>}
+                <Field type="text" name="name" placeholder="Name"/>
+            </div>
+            <div>
+                {touched.email && errors.email && <p>{errors.email}</p>}
+                <Field type="text" name="email" placeholder="email"/>
+            </div>
+            <div>
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                <Field type="text" name="password" placeholder="Password"/>
                 <button>Submit</button>
-            </Form>
+            </div>
+            <label>
+                <Field type="checkbox" name='ts' checked={values.ts} />
+                Accept ToS
+            </label>
+        </Form>
+        <div>
+
         </div>
     )
 }
@@ -22,9 +36,20 @@ const FormikUserForm = withFormik({
         return {
             name: name || '',
             email: email || '',
-            password: password || ''
+            password: password || '',
+            ts: ts || false
         }
     },
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+                .required('Name required'),
+        email: Yup.string()
+              .email('Invalid email')
+              .required('email required'),
+        password: Yup.string()
+                .min(6, 'Enter a password 6 characters or longer')
+                .required('Password required')
+    }),
     handleSubmit(values) {
         console.log(values);
     }
